5116412427dc8694b531a30949750dd971a6d353
Aleks <faerenpostcore@gmail.com>
2023-08-22 23:39:54 +0300

    Третье задание

---
+++ src/main/java/javaBasic/Task3.java
@@ -0,0 +1,14 @@
+package javaBasic;
+
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] arr = {1,7,13,5,6};
+        System.out.println(Arrays.toString(arr));
+        int foo = arr[0];
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = foo;
+        System.out.println(Arrays.toString(arr));
+    }
+}
